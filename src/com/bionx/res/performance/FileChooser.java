28bed259bbc23681015f14df0ba726097eeeed35
rgocal <rgocal09@gmail.com>
2013-09-30 10:45:57 -0400

    Nx Performance 2.0
    AOKP PC based
    Revamped for Droid Razr Family Kernel's and others

---
+++ src/com/bionx/res/performance/FileChooser.java
@@ -0,0 +1,307 @@
+package com.bionx.res.performance;
+
+import java.io.File;
+import java.sql.Date;
+import java.text.DateFormat;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.app.ListActivity;
+import android.app.ProgressDialog;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.content.res.Configuration;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.os.Environment;
+import android.preference.PreferenceManager;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import com.bionx.res.R;
+import com.bionx.res.util.Constants;
+import com.bionx.res.util.FileArrayAdapter;
+import com.bionx.res.util.Helpers;
+import com.bionx.res.util.Item;
+import com.bionx.res.util.UnzipUtility;
+
+public class FileChooser extends ListActivity implements Constants{
+    final Context context = this;
+    private File currentDir;
+    SharedPreferences mPreferences;
+    private FileArrayAdapter adapter;
+    private ProgressDialog progressDialog;
+    private String tip;
+    private String part;
+    private String nFile;
+    private int nbk=1;
+    private boolean iszip=false;
+    private String dtitlu;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        overridePendingTransition(R.anim.push_right_in, R.anim.push_right_out);
+        mPreferences = PreferenceManager.getDefaultSharedPreferences(this);
+        Intent intent1=getIntent();
+        tip=intent1.getStringExtra("mod");
+        part=intent1.getStringExtra("part");
+        if(tip.equalsIgnoreCase("kernel")){ dtitlu=getString(R.string.kernel_img_title);}
+        else{ dtitlu=getString(R.string.recovery_img_title);}
+        currentDir = new File(Environment.getExternalStorageDirectory().getAbsolutePath());
+        fill(currentDir);
+    }
+
+    @Override
+    public void onConfigurationChanged(Configuration newConfig) {
+        super.onConfigurationChanged(newConfig);
+        fill(currentDir);
+    }
+    @Override
+    public void onResume() {
+        super.onResume();
+    }
+
+    private void fill(File f){
+        File[]dirs = f.listFiles();
+        List<Item>dir = new ArrayList<Item>();
+        List<Item>fls = new ArrayList<Item>();
+        try{
+            for(File ff: dirs){
+                Date lastModDate = new Date(ff.lastModified());
+                DateFormat formater = DateFormat.getDateTimeInstance();
+                String date_modify = formater.format(lastModDate);
+                if(ff.isDirectory()){
+                    dir.add(new Item(ff.getName(),getString(R.string.dir),date_modify,ff.getAbsolutePath(),"dir"));
+                }
+                else{
+                    if((tip.equalsIgnoreCase("kernel") && ff.getName().equalsIgnoreCase("boot.img"))||(tip.equalsIgnoreCase("recovery") && ff.getName().equalsIgnoreCase("recovery.img")) || ff.getName().toLowerCase().endsWith(".zip"))
+                        fls.add(new Item(ff.getName(),Helpers.ReadableByteCount(ff.length()), date_modify, ff.getAbsolutePath(),"file"));
+                }
+            }
+        }
+        catch(Exception e){
+        }
+        Collections.sort(dir);
+        Collections.sort(fls);
+        dir.addAll(fls);
+        if(!f.getName().equalsIgnoreCase(""))
+        dir.add(0,new Item("..",getString(R.string.dir_parent),"",f.getParent(),"dir"));
+        adapter = new FileArrayAdapter(this,R.layout.file_view, dir);
+        this.setListAdapter(adapter);
+
+    }
+
+    @Override
+    public void onBackPressed(){
+        if(adapter.getItem(0).getName().equalsIgnoreCase("..")){
+            currentDir=currentDir.getParentFile();
+            fill(currentDir);
+            nbk=1;
+        }
+        else{
+            if(nbk==2){finish();}
+            else{
+                nbk++;
+                Toast.makeText(getApplicationContext(),getString(R.string.bkexit), Toast.LENGTH_SHORT).show();
+            }
+        }
+    }
+
+    @Override
+    protected void onListItemClick(ListView l, View v, int position, long id) {
+        Item o = adapter.getItem(position);
+        if(o.getImage().equalsIgnoreCase("dir")){
+            currentDir = new File(o.getPath());
+            fill(currentDir);
+        }
+        else{
+            nFile=currentDir+"/"+o.getName();
+            iszip=o.getName().toLowerCase().endsWith(".zip");
+            if(iszip){
+                new TestZipOperation().execute();
+            }
+            else{
+                makedialog();
+            }
+        }
+    }
+
+    private class TestZipOperation extends AsyncTask<String, Void, Boolean> {
+        @Override
+        protected Boolean doInBackground(String... params) {
+            final UnzipUtility unzipper = new UnzipUtility();
+            try{
+                return unzipper.testZip(nFile,tip);
+            }
+            catch (Exception e) {
+                Log.d(TAG,"ZIP error: "+nFile);
+                e.printStackTrace();
+                return false;
+            }
+        }
+        @Override
+        protected void onPostExecute(Boolean result) {
+            if (progressDialog != null) {
+                progressDialog.dismiss();
+            }
+            if(!result){
+                if(tip.equalsIgnoreCase("kernel")){
+                    Toast.makeText(context, getString(R.string.bad_zip,"boot.img"), Toast.LENGTH_SHORT).show();
+                }
+                else {
+                    Toast.makeText(context, getString(R.string.bad_zip,"recovery.img"), Toast.LENGTH_SHORT).show();
+                }
+                return;
+            }
+            makedialog();
+        }
+        @Override
+        protected void onPreExecute() {
+            progressDialog = ProgressDialog.show(FileChooser.this, null, getString(R.string.verify));
+        }
+        @Override
+        protected void onProgressUpdate(Void... values) {
+        }
+    }
+
+    private class FlashOperation extends AsyncTask<String, Void, String> {
+        @Override
+        protected String doInBackground(String... params) {
+            final StringBuilder sb = new StringBuilder();
+            final String dn=Environment.getExternalStorageDirectory().getAbsolutePath()+"/PerformanceControl/tmp";
+
+            if(tip.equalsIgnoreCase("kernel")){
+                if(iszip){
+                    try{
+                        new UnzipUtility().unzipfile(nFile,dn,"boot.img");
+                    }
+                    catch (Exception e) {
+                        Log.d(TAG,"unzip error: "+nFile);
+                        e.printStackTrace();
+                        return null;
+                    }
+                    nFile=dn+"/boot.img";
+                    if(part.contains("/dev/block/bml") && !Helpers.binExist("flash_image").equals(NOT_FOUND)){
+                        sb.append("flash_image boot "+nFile+"\n");
+                    }
+                    else{
+                        sb.append("dd if="+nFile+" of="+part+"\n");
+                    }
+                    sb.append("busybox rm -rf "+dn+"/*\n");
+                }
+                else{
+                    if(part.contains("/dev/block/bml") && !Helpers.binExist("flash_image").equals(NOT_FOUND)){
+                        sb.append("flash_image boot "+nFile+"\n");
+                    }
+                    else{
+                        sb.append("dd if="+nFile+" of="+part+"\n");
+                    }
+                    //sb.append("dd if="+nFile+" of="+part+"\n");
+                }
+                sb.append("busybox rm -rf /data/dalvik-cache/*\n");
+                sb.append("busybox rm -rf /cache/*\n");
+                sb.append("reboot\n");
+                //Log.d(TAG,sb.toString());
+            }
+            else{
+                if(iszip){
+                    try{
+                        new UnzipUtility().unzipfile(nFile,dn,"recovery.img");
+                    }
+                    catch (Exception e) {
+                        Log.d(TAG,"unzip error: "+nFile);
+                        e.printStackTrace();
+                        return null;
+                    }
+                    nFile=dn+"/recovery.img";
+                    if(part.contains("/dev/block/bml") && !Helpers.binExist("flash_image").equals(NOT_FOUND)){
+                        sb.append("flash_image recovery "+nFile+"\n");
+                    }
+                    else{
+                        sb.append("dd if="+nFile+" of="+part+"\n");
+                    }
+                    sb.append("busybox rm -rf "+dn+"/*\n");
+                }
+                else{
+                    if(part.contains("/dev/block/bml") && !Helpers.binExist("flash_image").equals(NOT_FOUND)){
+                        sb.append("flash_image recovery "+nFile+"\n");
+                    }
+                    else{
+                        sb.append("dd if="+nFile+" of="+part+"\n");
+                    }
+                    //sb.append("dd if="+nFile+" of="+part+"\n");
+                }
+
+                sb.append("reboot recovery\n");
+                //Log.d(TAG,sb.toString());
+            }
+            Helpers.shExec(sb);
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(String result) {
+            if (progressDialog != null) {
+                progressDialog.dismiss();
+            }
+        }
+
+        @Override
+        protected void onPreExecute() {
+            progressDialog = ProgressDialog.show(FileChooser.this, dtitlu, getString(R.string.wait));
+        }
+
+        @Override
+        protected void onProgressUpdate(Void... values) {
+        }
+    }
+
+    class CustomListener implements View.OnClickListener {
+        private final Dialog dialog;
+        public CustomListener(Dialog dialog) {
+            this.dialog = dialog;
+        }
+        @Override
+        public void onClick(View v) {
+            dialog.cancel();
+            new FlashOperation().execute();
+        }
+    }
+
+    private void makedialog(){
+        AlertDialog.Builder builder = new AlertDialog.Builder(context);
+        builder.setTitle(dtitlu)
+                .setMessage(nFile+" "+getString(R.string.flash_info,part)+" "+tip.toUpperCase()+"\n\n"+getString(R.string.wipe_cache_msg))
+                .setNegativeButton(getString(R.string.cancel),
+                        new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog,int id) {
+                                dialog.cancel();
+                                //finish();
+                            }
+                        })
+                .setPositiveButton(getString(R.string.yes),
+                        new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog, int id) {
+                                dialog.cancel();
+                            }
+                        });
+        ;
+        AlertDialog alertDialog = builder.create();
+        alertDialog.show();
+        //alertDialog.setCancelable(false);
+        Button theButton = alertDialog.getButton(DialogInterface.BUTTON_POSITIVE);
+        if (theButton != null) {
+            theButton.setOnClickListener(new CustomListener(alertDialog));
+        }
+    }
+
+}
---
+++ src/com/bionx/res/performance/PerformanceActivity.java
@@ -0,0 +1,205 @@
+package com.bionx.res.performance;
+
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.app.Fragment;
+import android.app.FragmentManager;
+import android.content.DialogInterface;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.preference.PreferenceManager;
+import android.support.v13.app.FragmentPagerAdapter;
+import android.support.v4.view.PagerTabStrip;
+import android.support.v4.view.ViewPager;
+
+import com.bionx.res.R;
+import com.bionx.res.activity.ShellInterface;
+import com.bionx.res.fragments.Advanced;
+import com.bionx.res.fragments.BatteryInfo;
+import com.bionx.res.fragments.CPUInfo;
+import com.bionx.res.fragments.CPUSettings;
+import com.bionx.res.fragments.DiskInfo;
+import com.bionx.res.fragments.OOMSettings;
+import com.bionx.res.fragments.TimeInState;
+import com.bionx.res.fragments.VoltageControlSettings;
+import com.bionx.res.util.Constants;
+import com.bionx.res.util.Helpers;
+
+public class PerformanceActivity extends Activity implements Constants {
+
+    SharedPreferences mPreferences;
+    PagerTabStrip mPagerTabStrip;
+    ViewPager mViewPager;
+
+    private static boolean mVoltageExists;
+
+    @Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+
+		if (!ShellInterface.isRooted()) {
+			AlertDialog.Builder builder = new AlertDialog.Builder(this);
+			builder.setTitle("Root Error");
+			builder.setMessage("SU Permissions were not found in your system and are required to continue...Are you rooted?");
+			builder.setCancelable(false);
+			builder.setPositiveButton("Exit",
+					new DialogInterface.OnClickListener() {
+						@Override
+						public void onClick(DialogInterface dialog, int which) {
+							finish();
+						}
+					});
+			builder.create().show();
+		} else if (!ShellInterface.hasSysfs()) {
+			AlertDialog.Builder builder = new AlertDialog.Builder(this);
+			builder.setTitle("Fatal Error");
+			builder.setMessage("CPU Overclock directory was not found...Cannot continue");
+			builder.setCancelable(false);
+			builder.setNegativeButton("Exit",
+					new DialogInterface.OnClickListener() {
+						public void onClick(DialogInterface dialog, int which) {
+							finish();
+						}
+					});
+			builder.create().show();
+		} else {
+        
+        setContentView(R.layout.nx_performance);
+        mPreferences = PreferenceManager.getDefaultSharedPreferences(this);
+        mVoltageExists = Helpers.voltageFileExists();
+
+        mViewPager = (ViewPager) findViewById(R.id.viewpager);
+        TitleAdapter titleAdapter = new TitleAdapter(getFragmentManager());
+        mViewPager.setAdapter(titleAdapter);
+        mViewPager.setCurrentItem(0);
+
+        mPagerTabStrip = (PagerTabStrip) findViewById(R.id.pagerTabStrip);
+        mPagerTabStrip.setBackgroundColor(getResources().getColor(R.color.transparent));
+        mPagerTabStrip.setTabIndicatorColor(getResources().getColor(R.color.holo_blue));
+        mPagerTabStrip.setDrawFullUnderline(true);
+
+	    Helpers.shCreate();
+    }
+    }
+
+    class TitleAdapter extends FragmentPagerAdapter {
+        String titles[] = getTitles();
+        private Fragment frags[] = new Fragment[titles.length];
+
+        public TitleAdapter(FragmentManager fm) {
+            super(fm);
+            if (mVoltageExists) {
+            	if(Helpers.showBattery()){
+	                frags[0] = new CPUSettings();
+		            frags[1] = new BatteryInfo();
+		            frags[2] = new OOMSettings();
+	                frags[3] = new VoltageControlSettings();
+	                frags[4] = new Advanced();
+	                frags[5] = new TimeInState();
+	                frags[6] = new CPUInfo();
+                    frags[7] = new DiskInfo();
+            	}
+            	else{
+			        frags[0] = new CPUSettings();
+	        	    frags[1] = new OOMSettings();
+                	frags[2] = new VoltageControlSettings();
+                	frags[3] = new Advanced();
+                	frags[4] = new TimeInState();
+                	frags[5] = new CPUInfo();
+                    frags[6] = new DiskInfo();
+            	}
+            } 
+            else {
+                if(Helpers.showBattery()){
+                    frags[0] = new CPUSettings();
+                    frags[1] = new BatteryInfo();
+                    frags[2] = new OOMSettings();
+                    frags[3] = new Advanced();
+                    frags[4] = new TimeInState();
+                    frags[5] = new CPUInfo();
+                    frags[6] = new DiskInfo();
+                }
+                else{
+                    frags[0] = new CPUSettings();
+                    frags[1] = new OOMSettings();
+                    frags[2] = new Advanced();
+                    frags[3] = new TimeInState();
+                    frags[4] = new CPUInfo();
+                    frags[5] = new DiskInfo();
+                }
+            }
+        }
+
+        @Override
+        public CharSequence getPageTitle(int position) {
+            return titles[position];
+        }
+
+        @Override
+        public Fragment getItem(int position) {
+            return frags[position];
+        }
+
+        @Override
+        public int getCount() {
+            return frags.length;
+        }
+
+    /**
+     * Get a list of titles for the tabstrip to display depending on if the
+     * voltage control fragment and battery fragment will be displayed. (Depends on the result of
+     * Helpers.voltageTableExists() & Helpers.showBattery()
+     *
+     * @return String[] containing titles
+     */
+    private String[] getTitles() {
+        String titleString[];
+        if (mVoltageExists) {
+        	if(Helpers.showBattery()){
+                titleString = new String[]{
+                        getString(R.string.t_cpu_settings),
+                        getString(R.string.t_battery_info),
+                        getString(R.string.t_oom_settings),
+                        getString(R.string.t_volt_settings),
+                        getString(R.string.t_adv_settings),
+                        getString(R.string.t_time_in_state),
+                        getString(R.string.t_cpu_info),
+                        getString(R.string.t_disk_info)};
+                }
+                else{
+                    titleString = new String[]{
+                            getString(R.string.t_cpu_settings),
+                            getString(R.string.t_oom_settings),
+                            getString(R.string.t_volt_settings),
+                            getString(R.string.t_adv_settings),
+                            getString(R.string.t_time_in_state),
+                            getString(R.string.t_cpu_info),
+                            getString(R.string.t_disk_info)};
+                }
+        } 
+        else {
+        	if(Helpers.showBattery()){
+                titleString = new String[]{
+                        getString(R.string.t_cpu_settings),
+                        getString(R.string.t_battery_info),
+                        getString(R.string.t_oom_settings),
+                        getString(R.string.t_adv_settings),
+                        getString(R.string.t_time_in_state),
+                        getString(R.string.t_cpu_info),
+                        getString(R.string.t_disk_info)};
+            }
+        	else{
+                titleString = new String[]{
+                        getString(R.string.t_cpu_settings),
+                        getString(R.string.t_oom_settings),
+                        getString(R.string.t_adv_settings),
+                        getString(R.string.t_time_in_state),
+                        getString(R.string.t_cpu_info),
+                        getString(R.string.t_disk_info)};
+            }
+        }
+        return titleString;
+    }
+    }
+}
+    
